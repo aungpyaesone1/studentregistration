b3369672f935ac14aee089b1af368af5e898506c
aungpyae.sone <aungpyae.sone@yomabank.com>
2023-02-13 16:34:34 +0630

    test1 endpoint

--- src/main/java/com/example/registration/Controller/StudentController.java
+++ src/main/java/com/example/registration/Controller/StudentController.java
@@ -106,4 +106,9 @@ public class StudentController {
         res.setSyskey(syskey);
         return ResponseEntity.ok().body(res);
     }
+
+    @GetMapping("test1")
+    public ResponseEntity<?> test1(){
+        return ResponseEntity.ok().body("Test1");
+    }
 }
